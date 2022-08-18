ac587fc01f3a3589f060dad0a5a727bb5290c2a8
Emily Fernschild <emilyfernschild@gmail.com>
2022-08-18 14:28:15 -0400

    added button

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 const h3 = document.createElement("h3");
 h3.textContent = "Flatiron School Student";
 document.querySelector("body").appendChild(h3);
+
+function myFunc() {
+const bio = document.querySelector("#myBio");
+  if (bio.classList.contains("hide")) {
+    bio.classList.toggle("hide");
+  }
+  else {
+    bio.classList.remove("hide")
+  }
+};
+
